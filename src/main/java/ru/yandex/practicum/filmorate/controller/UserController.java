004d499205835c6835b324f29e1b25f1b4071862
Ali Ibragim <ibragimali768@gmail.com>
2025-05-16 07:39:02 +0300

    Prj10prt1

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,42 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exepcions.сustom.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+import jakarta.validation.Valid;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int idCounter = 1;
+
+    @GetMapping
+    public Collection<User> findAll() {
+        log.info("Getting all users");
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        user.setId(idCounter++);
+        users.put(user.getId(), user);
+        log.info("Created new user: {}", user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody User user) {
+        if (!users.containsKey(user.getId())) {
+            log.error("User with id {} not found", user.getId());
+            throw new NotFoundException("User not found");
+        }
+        users.put(user.getId(), user);
+        log.info("Updated user: {}", user);
+        return user;
+    }
+}
