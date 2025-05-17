f5ae8dfdee51ecc1523e20c6300de231213b0c05
Ali Ibragim <ibragimali768@gmail.com>
2025-05-17 07:51:08 +0300

    Prj10pt1

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,32 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.release.film.MinReleaseDate;
+import java.time.LocalDate;
+
+
+
+@Data
 public class Film {
+    private int id;
+
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов")
+    private String description;
+
+    @MinReleaseDate
+    @NotNull(message = "Дата релиза обязательна")
+    @PastOrPresent(message = "Дата релиза не может быть в будущем")
+    private LocalDate releaseDate;
+
+    @Positive(message = "Продолжительность фильма должна быть положительной")
+    private int duration;
 }
